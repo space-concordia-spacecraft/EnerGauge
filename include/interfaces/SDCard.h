aec382f32a713b0818f88c9e0af17d450520d43c
William Wells <williamwells2013@hotmail.com>
2023-07-01 17:20:33 -0400

    Add no OS sd card library as dep

---
+++ include/interfaces/SDCard.h
@@ -0,0 +1,20 @@
+//
+// Created by william on 01/07/23.
+//
+
+#ifndef SC_BATTERY_TEST_JIG_SDCARD_H
+#define SC_BATTERY_TEST_JIG_SDCARD_H
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "sd_card.h"
+#include "ff.h"
+
+class SDCard {
+public:
+    SDCard();
+
+};
+
+#endif //SC_BATTERY_TEST_JIG_SDCARD_H
---
+++ src/interfaces/SDCard.cpp
@@ -0,0 +1,10 @@
+//
+// Created by william on 01/07/23.
+//
+
+#include "interfaces/SDCard.h"
+
+SDCard::SDCard() {
+
+}
+
